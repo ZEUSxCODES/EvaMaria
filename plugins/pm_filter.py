6f1d2b53607e613450ff9c35df72c44298afa600
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-02-20 12:33:46 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -668,6 +668,20 @@ async def auto_filter(client, msg, spoll=False):
             ]
             for file in files
         ]
+    else:
+        btn = [
+            [
+                InlineKeyboardButton(
+                    text=f"{file.file_name}",
+                    callback_data=f'{pre}#{file.file_id}',
+                ),
+                InlineKeyboardButton(
+                    text=f"{get_size(file.file_size)}",
+                    callback_data=f'{pre}#{file.file_id}',
+                ),
+            ]
+            for file in files
+        ]
 
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
